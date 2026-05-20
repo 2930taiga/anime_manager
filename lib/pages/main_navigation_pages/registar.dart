320898d60bac5de2c1c0a0b460b90440595de4ef
Taiga2930 <70kafu@gmail.com>
2026-05-20 13:50:00 +0900

    riverpodを使って入力中のデータを保持するためのクラスを宣言した．

--- lib/pages/main_navigation_pages/registar.dart
+++ lib/pages/main_navigation_pages/registar.dart
@@ -61,26 +61,26 @@ class RegistarPage extends StatelessWidget {
   }
 }
 
-//ステータスのenum
-enum Status{
-  before,
-  watching,
-  complete,
-  stop,
-  pause
-}
+// //ステータスのenum
+// enum Status{
+//   before,
+//   watching,
+//   complete,
+//   stop,
+//   pause
+// }
   
-//入力中のデータを保持しておくクラスを宣言
-class AnimeInputData{
-  Status status = Status.before; //ステータス 
-  String title = ""; //タイトル
-  String titleKana = ""; //タイトル（かな）
-  String date = ""; //日付
-  int epNum = 0; //話数
-  String epTime =""; //1話あたりの時間
-  int evaliation = 0; //評価
-  String memo=""; //メモ
-}
+// //入力中のデータを保持しておくクラスを宣言
+// class AnimeInputData{
+//   Status status = Status.before; //ステータス 
+//   String title = ""; //タイトル
+//   String titleKana = ""; //タイトル（かな）
+//   String date = ""; //日付
+//   int epNum = 0; //話数
+//   String epTime =""; //1話あたりの時間
+//   int evaliation = 0; //評価
+//   String memo=""; //メモ
+// }
 
 
 class TVRegistar extends ConsumerStatefulWidget {
