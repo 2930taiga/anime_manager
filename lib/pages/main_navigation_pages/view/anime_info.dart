971bed4bbf49f80bd591e9e750edc8bac41e9548
Taiga2930 <70kafu@gmail.com>
2026-06-05 12:21:51 +0900

    アニメ情報ページでアニメの詳細表時/非表示ボタンを作成．タイトルとタイトルかなを配置した．

--- lib/pages/main_navigation_pages/view/anime_info.dart
+++ lib/pages/main_navigation_pages/view/anime_info.dart
@@ -22,49 +22,132 @@ class _AnimeInfoState extends State<AnimeInfo> {
   double containerPaddingHorizontal = 5;
   double containerPaddingVertical = 5;
 
+  //詳細情報を表示するかどうか
+  bool detailInfo = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.anime.title),
+        leading: IconButton( //戻るボタン
+          onPressed:(){
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back)
+        ),
+
+        title: Text(widget.anime.title), //タイトル
+
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.more_vert_outlined)
+          )
+        ],
       ),
       body: Column(
         children: [
-          Padding( //タイトルその他
-            padding: EdgeInsetsGeometry.symmetric(
-              horizontal: cardPaddingHorizontal,
-              vertical: cardPaddingVertical
-            ),
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: containerPaddingHorizontal,
-                vertical: containerPaddingVertical
-              ),
-              child: Column(
+          SizedBox(height: 20,),
+          Expanded(
+            child: Scrollbar(
+              child: ListView(
                 children: [
-                  Text(
-                    "data",
-                    style: TextStyle(
-                      fontSize: 20
+                  Padding( //タイトルその他
+                    padding: EdgeInsetsGeometry.symmetric(
+                      horizontal: cardPaddingHorizontal,
+                      vertical: cardPaddingVertical
                     ),
-                  )
+                    child: Container(
+                      width: double.infinity,
+                      //height: 500,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: containerPaddingHorizontal,
+                        vertical: containerPaddingVertical
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text( //タイトル
+                                      widget.anime.title,
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold
+                                      ),
+                                    ),
+                                    if(detailInfo==true)
+                                    Text(
+                                      widget.anime.titleKana,
+                                      style: TextStyle(
+                                        fontSize: 15
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ),
+                              IconButton( //詳細表示/非表示ボタン
+                                onPressed: (){
+                                  setState(() {
+                                    if(detailInfo==true){
+                                      detailInfo=false;
+                                    }
+                                    else{
+                                      detailInfo=true;
+                                    }
+                                  });
+                                },
+                                icon: detailInfo
+                                ? Icon(Icons.expand_less)
+                                : Icon(Icons.expand_more)
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding( //タイトルその他
+                    padding: EdgeInsetsGeometry.symmetric(
+                      horizontal: cardPaddingHorizontal,
+                      vertical: cardPaddingVertical
+                    ),
+                    child: Container(
+                      width: double.infinity,
+                      //height: 500,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: containerPaddingHorizontal,
+                        vertical: containerPaddingVertical
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            "data",
+                            style: TextStyle(
+                              fontSize: 20
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
-              ),
-            ),
+              )
+            )
           ),
-          Padding( //ジャンル
-            padding: EdgeInsetsGeometry.symmetric(
-              horizontal: cardPaddingHorizontal,
-              vertical: cardPaddingVertical
-            ),
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: containerPaddingHorizontal,
-                vertical: containerPaddingVertical
-              ),
-              child: Text("ここにジャンル"),
-            ),
-          ),
+          
         ],
       ),
     );
